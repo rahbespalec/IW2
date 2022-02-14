70ac36460327dde4949338fa3992e82df14f058b
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2022-02-14 11:09:23 -0300

    Delete livrinhos directory

--- livrinhos/js/functions.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.navbar ul.dropdown-menu li').click(function(){
-        let section = $(this).attr('section')
-        let scrollTop = $('section[section='+section+']').offset().top
-        $('html, body').animate({scrollTop:scrollTop}, 100)
-    })
-})
-
